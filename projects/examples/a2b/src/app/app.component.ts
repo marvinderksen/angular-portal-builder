917d241b13bd3dfe3010e4cf8def73c55bcde4f5
Marvin Derksen <marvin.derksen@lise.de>
2021-08-27 14:53:13 +0200

    update dependencies to work with angular 12

--- projects/examples/a2b/src/app/app.component.ts
+++ projects/examples/a2b/src/app/app.component.ts
@@ -6,28 +6,28 @@ import { trips } from './data';
 import { Trip } from './types';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  private readonly fromStream = new Subject<string>();
-  private readonly toStream = new Subject<string>();
+    private readonly fromStream = new Subject<string>();
+    private readonly toStream = new Subject<string>();
 
-  readonly trip: Observable<Trip | undefined>;
+    readonly trip: Observable<Trip | undefined>;
 
-  constructor() {
-    this.trip = combineLatest([this.fromStream, this.toStream]).pipe(
-      map(([from, to]) => trips.find((it) => it.from === from && it.to === to)),
-      startWith(undefined)
-    );
-  }
+    constructor() {
+        this.trip = combineLatest([this.fromStream, this.toStream]).pipe(
+            map(([from, to]) => trips.find((it) => it.from === from && it.to === to)),
+            startWith<Trip | undefined>(undefined)
+        );
+    }
 
-  selectFrom(city: string) {
-    this.fromStream.next(city);
-  }
+    selectFrom(city: string) {
+        this.fromStream.next(city);
+    }
 
-  selectTo(city: string) {
-    this.toStream.next(city);
-  }
+    selectTo(city: string) {
+        this.toStream.next(city);
+    }
 }
