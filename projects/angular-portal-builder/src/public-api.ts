9a5446b2a129deb67d43fc12c357db0aecb2fffe
Marvin Derksen <marvin.derksen@lise.de>
2021-08-30 10:28:36 +0200

    export default token as public api

--- projects/angular-portal-builder/src/public-api.ts
+++ projects/angular-portal-builder/src/public-api.ts
@@ -7,3 +7,4 @@ export * from './lib/component-registry';
 export * from './lib/component-registration';
 export * from './lib/invalid-operation';
 export * from './lib/no-suitable-registration';
+export * from './lib/default-token';
